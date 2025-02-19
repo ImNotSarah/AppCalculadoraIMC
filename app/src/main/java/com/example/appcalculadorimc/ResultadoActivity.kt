e97540aa802485efa26dc45c6877611c445f08f9
Sarah <sarahdejesuslima07@gmail.com>
2025-02-19 19:56:20 -0300

    Adicionado Aula Criando interface

--- app/src/main/java/com/example/appcalculadorimc/ResultadoActivity.kt
+++ app/src/main/java/com/example/appcalculadorimc/ResultadoActivity.kt
@@ -1,32 +1,20 @@
 package com.example.appcalculadorimc
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
-
-    private lateinit var btnCalcular: Button
-
+class ResultadoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_resultado)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        btnCalcular = findViewById(R.id.btn_calcular)
-
-        btnCalcular.setOnClickListener {
-            val intent = Intent(this, ResultadoActivity::class.java)
-
-            startActivity(intent)
         }
     }
 }
